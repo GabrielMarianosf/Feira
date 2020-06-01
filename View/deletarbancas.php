070b62c5720c0140725366583f15c69a11ccfb75
Gabriel <cfaltribim@gmail.com>
2020-06-01 20:36:40 -0300

    atual

---
+++ View/deletarbancas.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<body>
+    <form action="../controler/cadastrarbanca.php" method="POST">
+            <center>
+                
+                <b>Númedo do Box: *</b> <input type="text" id="box" name="box" placeholder="Digite o Box"
+                    required /><br><br>
+                
+                <div class="g-recaptcha" data-sitekey="6LdXS_oUAAAAAIpAaqaWH7RlUhsWFi_GbPUWZdcm"></div>
+                <input type="submit" value="Enviar" class="btn btn-primary" name="Cadastrar">
+            </center>
+        </form><br/>
+
+        <div id="conteudo_edbanca">
+        </div>
+
+        
+</body>
+</html>
--- View/editarbancas.php
+++ View/editarbancas.php
@@ -3,12 +3,30 @@
     <form action="../controler/cadastrarbanca.php" method="POST">
             <center>
                 
+                <h1>Editar uma Banca </h1>
+                
                 <b>Númedo do Box: *</b> <input type="text" id="box" name="box" placeholder="Digite o Box"
                     required /><br><br>
-                
                 <div class="g-recaptcha" data-sitekey="6LdXS_oUAAAAAIpAaqaWH7RlUhsWFi_GbPUWZdcm"></div>
                 <input type="submit" value="Enviar" class="btn btn-primary" name="Cadastrar">
             </center>
         </form>
+        <?php
+
+            include_once("../Model/conecta.php");
+        
+            $sql = "select * from banca";
+            $resultado = mysqli_query($conexao, $sql);
+            while ($row = mysqli_fetch_assoc($resultado)) {
+                echo "ID: " . $row['id']. "<br/>";
+                echo "Nome: " . $row['nome']. "<br/>";
+                echo "Categoria: " . $row['categoria']. "<br/>";
+                echo "Telefone: " . $row['telefone']. "<br/>";
+                echo "Box: " . $row['box']. "<br/>";
+                echo "E-mail: " . $row['email']. "<br/>";
+                echo "Descrição: " . $row['descricao']. "<br/><hr>";
+            }
+
+        ?>
 </body>
 </html>
