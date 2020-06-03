f40ec81a5bf1e2b2f76b2e34e46f13814655ba87
Gabriel <cfaltribim@gmail.com>
2020-06-03 19:39:40 -0300

    editar quase funcionando

--- View/editar_bancas.php
+++ View/editar_bancas.php
@@ -3,7 +3,7 @@
 session_start();
 include_once("../Model/conecta.php");
 $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
-$result_usuario = "SELECT * FROM usuarios WHERE id = '$id'";
+$result_usuario = "SELECT * FROM banca WHERE id = '$id'";
 $resultado_usuario = mysqli_query($conexao, $result_usuario);
 $row_usuario = mysqli_fetch_assoc($resultado_usuario);
 
@@ -22,11 +22,11 @@ $row_usuario = mysqli_fetch_assoc($resultado_usuario);
 <body>
     <form action="../controler/cadastrarbanca.php" method="POST">
             <center>
-                <b>Nome da banca: *</b> <input type="text" id="nome" name="nome" placeholder="Digite o nome da banca"
+                <b>Nome da banca: *</b> <input type="text" value="<?php echo $row_usuario['nome'];?>" id="nome" name="nome" placeholder="Digite o nome da banca"
                     required /><br><br>
                 <b>Categoria da banca *</b>
                 <select name="categoria" id="categoria">
-                    <option value="Selecione uma categoria" selected>-- Selecione uma Categoria --</option>
+                    <option value="<?php echo $row_usuario['categoria'];?>" selected><?php echo $row_usuario['categoria'];?></option>
                     <option value="Brinquedos">Brinquedos</option>
                     <option value="Eletrodomesticos">Eletrodomésticos</option>
                     <option value="Eletronicos">Eletrônicos</option>
@@ -35,16 +35,15 @@ $row_usuario = mysqli_fetch_assoc($resultado_usuario);
                     <option value="Infantil">Infantil</option>
                 </select>
                 <br><br>
-                <b>Telefone: *</b> <input type="text" id="telefone" name="telefone" placeholder="Contato da banca"
+                <b>Telefone: *</b> <input type="text" value="<?php echo $row_usuario['telefone'];?>" id="telefone" name="telefone" placeholder="Contato da banca"
                     required /><br><br>
-                <b>Númedo do Box: *</b> <input type="text" id="box" name="box" placeholder="Digite o Box"
+                <b>Númedo do Box: *</b> <input type="text" value="<?php echo $row_usuario['box'];?>" id="box" name="box" placeholder="Digite o Box"
                     required /><br><br>
-                <b>E-mail: *</b> <input type="email" class="form-control-inline" id="email" name="email"
+                <b>E-mail: *</b> <input type="email" value="<?php echo $row_usuario['email'];?>" class="form-control-inline" id="email" name="email"
                     placeholder="exemplo@exemplo.com" required /><br><br>
                 <b>Descrição da banca: * <br></b> <textarea id="descricao" name="descricao" rows="5" cols="80"
-                    required></textarea><br><br>
-                <div class="g-recaptcha" data-sitekey="6LdXS_oUAAAAAIpAaqaWH7RlUhsWFi_GbPUWZdcm"></div>
-                <input type="submit" value="Enviar" class="btn btn-primary" name="Cadastrar">
+                    required><?php echo $row_usuario['descricao'];?></textarea><br><br>
+                <input type="submit" value="Editar" class="btn btn-primary" name="Cadastrar">
             </center>
         </form>
     
